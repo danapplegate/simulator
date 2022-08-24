46b50335a44a618cfa9a60cfd9385e989ca2f0ae
Dan Applegate <applegatedt@gmail.com>
2022-08-24 02:27:42 -0400

    Simplify Force trait definition

--- src/force.rs
+++ src/force.rs
@@ -3,15 +3,15 @@ use crate::{Massive, Positioned};
 
 const G: f64 = 6.67430e-11;
 
-pub trait Force<'a, T> {
+pub trait Force<T> {
     // Returns the magnitude of the force calculated between
     // two objects of type T.
-    fn calculate_between(lhs: &'a T, rhs: &'a T) -> f64;
+    fn calculate_between(lhs: T, rhs: T) -> f64;
 }
 
 pub struct Gravity {}
 
-impl<'a, T> Force<'a, T> for Gravity
+impl<'a, T> Force<&'a T> for Gravity
 where
     T: Positioned<'a> + Massive,
 {
